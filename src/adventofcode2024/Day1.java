870aced9c7a75820372f3800d4e3ec9709910baf
biancat9 <btuliaodev@gmail.com>
2024-12-03 22:38:37 -0600

    complete day 1

--- src/adventofcode2024/Day1.java
+++ src/adventofcode2024/Day1.java
@@ -10,7 +10,6 @@ public class Day1 {
 		Scanner scan = new Scanner(locationIDList);
 		ArrayList<Integer> leftLocationIDList = new ArrayList<Integer>();
 		ArrayList<Integer> rightLocationIDList = new ArrayList<Integer>();
-		ArrayList<Integer> distanceApartList = new ArrayList<Integer>();
 		int totalDistance = 0;
 		
 		while (scan.hasNextLine()) {
@@ -23,14 +22,23 @@ public class Day1 {
 		Collections.sort(leftLocationIDList);
 		Collections.sort(rightLocationIDList);
 		
+		// finds total distance by the sum of the difference between smallest numbers on left and right lists
 		for (int i = 0; i < leftLocationIDList.size(); i++) {
-			distanceApartList.add(Math.abs(leftLocationIDList.get(i) - rightLocationIDList.get(i)));
+			totalDistance += Math.abs(leftLocationIDList.get(i) - rightLocationIDList.get(i));
 		}
-		
-		for (Integer currentDistanceApart : distanceApartList) {
-			totalDistance += currentDistanceApart;
-		}
-	
 		System.out.println(totalDistance);
+		
+		// finds similarity score by sum of multiplying each number from the left list to the number of its occurrences in the right list
+		int similarityScore = 0;
+		for (Integer currentLeftLocationID : leftLocationIDList) {
+			int numOfAppearances = 0;
+			for (int i = 0; i < rightLocationIDList.size(); i++) {
+				if (currentLeftLocationID.equals(rightLocationIDList.get(i))) {
+					numOfAppearances++;
+				}
+			}
+			similarityScore += currentLeftLocationID * numOfAppearances;
+		}
+		System.out.println(similarityScore);
 	}
 }
